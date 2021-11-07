985fc81c1d3e7c33bc970614e79c9b054e671df9
imroatul faizah <faizah.alfaza@gmail.com>
2021-11-07 21:13:16 +0700

    backup

--- db_pm.sql
+++ db_pm.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 4.7.9
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Jan 06, 2019 at 11:48 PM
--- Server version: 10.1.37-MariaDB
--- PHP Version: 7.3.0
+-- Host: 127.0.0.1:3306
+-- Generation Time: Oct 19, 2021 at 07:30 PM
+-- Server version: 5.7.21
+-- PHP Version: 7.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,10 +19,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `db_spk_pm`
+-- Database: `db_pm`
 --
-CREATE DATABASE IF NOT EXISTS `db_spk_pm` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `db_spk_pm`;
 
 -- --------------------------------------------------------
 
@@ -30,10 +28,12 @@ USE `db_spk_pm`;
 -- Table structure for table `tb_alternatif`
 --
 
-CREATE TABLE `tb_alternatif` (
+DROP TABLE IF EXISTS `tb_alternatif`;
+CREATE TABLE IF NOT EXISTS `tb_alternatif` (
   `id_alternatif` varchar(70) NOT NULL,
-  `kode_alternatif` varchar(10) NOT NULL,
-  `nama_alternatif` varchar(150) NOT NULL
+  `kode_alternatif` varchar(100) NOT NULL,
+  `nama_alternatif` varchar(150) NOT NULL,
+  PRIMARY KEY (`id_alternatif`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -41,10 +41,11 @@ CREATE TABLE `tb_alternatif` (
 --
 
 INSERT INTO `tb_alternatif` (`id_alternatif`, `kode_alternatif`, `nama_alternatif`) VALUES
-('5c07ab930d6b5', 'A1', 'Alternatif 1'),
-('5c07abb380826', 'A2', 'Alternatif 2'),
-('5c07ac27a9f7f', 'A3', 'Alternatif 3'),
-('5c235a78768d1', 'A4', 'Alternatif 4');
+('5c07ab930d6b5', '5315032304920001', 'WIHELMUS MEMO'),
+('5c07abb380826', '5315030308890001', 'PRIMUS ADI NGATOM'),
+('5c07ac27a9f7f', '5315036510950001', 'MELANIA BUPU'),
+('5c235a78768d1', '5315076607940002', 'AVELINA MAYA'),
+('615c83b45a65f', '5315037006920001', 'EMILIYANA JENGONG ');
 
 -- --------------------------------------------------------
 
@@ -52,11 +53,13 @@ INSERT INTO `tb_alternatif` (`id_alternatif`, `kode_alternatif`, `nama_alternati
 -- Table structure for table `tb_aspek`
 --
 
-CREATE TABLE `tb_aspek` (
+DROP TABLE IF EXISTS `tb_aspek`;
+CREATE TABLE IF NOT EXISTS `tb_aspek` (
   `id_aspek` varchar(70) NOT NULL,
   `kode_aspek` varchar(6) NOT NULL,
   `nama_aspek` varchar(150) NOT NULL,
-  `bobot` int(3) NOT NULL
+  `bobot` int(3) NOT NULL,
+  PRIMARY KEY (`id_aspek`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -64,10 +67,10 @@ CREATE TABLE `tb_aspek` (
 --
 
 INSERT INTO `tb_aspek` (`id_aspek`, `kode_aspek`, `nama_aspek`, `bobot`) VALUES
-('5c0a2a2167452', 'JK', 'Jarak', 10),
-('5c0a2a3b87d03', 'L', 'Lokasi', 20),
-('5c0a2a5902c8d', 'T', 'Transportasi', 10),
-('5c0a2a7074dc9', 'I', 'Infrastruktur', 20);
+('5c0a2a2167452', 'K04', 'STATUS PEKERJAAN', 20),
+('5c0a2a3b87d03', 'K02', 'STATUS PENDUDUK', 20),
+('5c0a2a5902c8d', 'K03', 'KELUARGA TIDAK MAMPU', 30),
+('5c0a2a7074dc9', 'K01', 'STATUS PENERIMA', 30);
 
 -- --------------------------------------------------------
 
@@ -75,10 +78,13 @@ INSERT INTO `tb_aspek` (`id_aspek`, `kode_aspek`, `nama_aspek`, `bobot`) VALUES
 -- Table structure for table `tb_hasil`
 --
 
-CREATE TABLE `tb_hasil` (
+DROP TABLE IF EXISTS `tb_hasil`;
+CREATE TABLE IF NOT EXISTS `tb_hasil` (
   `id_hasil` varchar(70) NOT NULL,
   `id_alternatif` varchar(70) NOT NULL,
-  `hasil` double NOT NULL
+  `hasil` double NOT NULL,
+  PRIMARY KEY (`id_hasil`),
+  KEY `fk_hasil_alternatif` (`id_alternatif`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -86,10 +92,11 @@ CREATE TABLE `tb_hasil` (
 --
 
 INSERT INTO `tb_hasil` (`id_hasil`, `id_alternatif`, `hasil`) VALUES
-('5c30b4aba38a0', '5c07ab930d6b5', 1.43),
-('5c30b4abb0e83', '5c07abb380826', 1.39),
-('5c30b4abbea15', '5c07ac27a9f7f', 1.2225),
-('5c30b4abcc1ec', '5c235a78768d1', 1.5375);
+('616eabee1950d', '5c07ab930d6b5', 1.5633333333333),
+('616eabee24b06', '5c07abb380826', 1.5633333333333),
+('616eabee291a4', '5c07ac27a9f7f', 1.5633333333333),
+('616eabee2caea', '5c235a78768d1', 1.5633333333333),
+('616eabee31485', '615c83b45a65f', 1.5633333333333);
 
 -- --------------------------------------------------------
 
@@ -97,11 +104,14 @@ INSERT INTO `tb_hasil` (`id_hasil`, `id_alternatif`, `hasil`) VALUES
 -- Table structure for table `tb_penilaian`
 --
 
-CREATE TABLE `tb_penilaian` (
+DROP TABLE IF EXISTS `tb_penilaian`;
+CREATE TABLE IF NOT EXISTS `tb_penilaian` (
   `id_penilaian` varchar(70) NOT NULL,
   `id_alternatif` varchar(70) NOT NULL,
   `id_sub_kriteria` varchar(70) NOT NULL,
-  `skor` int(3) NOT NULL
+  `skor` int(3) NOT NULL,
+  KEY `fk_sub_kriteria` (`id_sub_kriteria`),
+  KEY `fk_alternatif` (`id_alternatif`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -109,38 +119,61 @@ CREATE TABLE `tb_penilaian` (
 --
 
 INSERT INTO `tb_penilaian` (`id_penilaian`, `id_alternatif`, `id_sub_kriteria`, `skor`) VALUES
-('5c30b46376e31', '5c07ab930d6b5', '5c0f0b4f7d558', 6),
-('5c30b46376e43', '5c07ab930d6b5', '5c0f0b6cb7f6a', 7),
-('5c30b46376e46', '5c07ab930d6b5', '5c0f0bd7f11e5', 4),
-('5c30b46376e48', '5c07ab930d6b5', '5c0f0bf5353b4', 7),
-('5c30b46376e4b', '5c07ab930d6b5', '5c0f0c1481601', 5),
-('5c30b46376e4c', '5c07ab930d6b5', '5c0f0c2a2c71c', 5),
-('5c30b46376e4e', '5c07ab930d6b5', '5c0f0c4f5b6af', 5),
-('5c30b46376e51', '5c07ab930d6b5', '5c0f0c7ee072a', 7),
-('5c30b46376e54', '5c07abb380826', '5c0f0b4f7d558', 4),
-('5c30b46376e5a', '5c07abb380826', '5c0f0b6cb7f6a', 4),
-('5c30b46376e5b', '5c07abb380826', '5c0f0bd7f11e5', 2),
-('5c30b46376e62', '5c07abb380826', '5c0f0bf5353b4', 7),
-('5c30b46376e63', '5c07abb380826', '5c0f0c1481601', 7),
-('5c30b46376e64', '5c07abb380826', '5c0f0c2a2c71c', 7),
-('5c30b46376e65', '5c07abb380826', '5c0f0c4f5b6af', 6),
-('5c30b46376e67', '5c07abb380826', '5c0f0c7ee072a', 7),
-('5c30b46376e68', '5c07ac27a9f7f', '5c0f0b4f7d558', 7),
-('5c30b46376e69', '5c07ac27a9f7f', '5c0f0b6cb7f6a', 7),
-('5c30b46376e6a', '5c07ac27a9f7f', '5c0f0bd7f11e5', 7),
-('5c30b46376e6b', '5c07ac27a9f7f', '5c0f0bf5353b4', 7),
-('5c30b46376e6c', '5c07ac27a9f7f', '5c0f0c1481601', 3),
-('5c30b46376e6d', '5c07ac27a9f7f', '5c0f0c2a2c71c', 3),
-('5c30b46376e6e', '5c07ac27a9f7f', '5c0f0c4f5b6af', 3),
-('5c30b46376e6f', '5c07ac27a9f7f', '5c0f0c7ee072a', 5),
-('5c30b46376e70', '5c235a78768d1', '5c0f0b4f7d558', 4),
-('5c30b46376e71', '5c235a78768d1', '5c0f0b6cb7f6a', 4),
-('5c30b46376e72', '5c235a78768d1', '5c0f0bd7f11e5', 7),
-('5c30b46376e74', '5c235a78768d1', '5c0f0bf5353b4', 7),
-('5c30b46376e76', '5c235a78768d1', '5c0f0c1481601', 7),
-('5c30b46376e77', '5c235a78768d1', '5c0f0c2a2c71c', 7),
-('5c30b46376e79', '5c235a78768d1', '5c0f0c4f5b6af', 6),
-('5c30b46376e7a', '5c235a78768d1', '5c0f0c7ee072a', 6);
+('616d642a7cca5', '5c07ab930d6b5', '5c0f0b4f7d558', 5),
+('616d642a7ccb0', '5c07ab930d6b5', '5c0f0b6cb7f6a', 5),
+('616d642a7ccb5', '5c07ab930d6b5', '5c0f0bd7f11e5', 5),
+('616d642a7ccbb', '5c07ab930d6b5', '5c0f0bf5353b4', 5),
+('616d642a7ccbf', '5c07ab930d6b5', '616d54c781464', 5),
+('616d642a7ccc4', '5c07ab930d6b5', '616d548e74f98', 5),
+('616d642a7ccc9', '5c07ab930d6b5', '5c0f0c1481601', 5),
+('616d642a7ccce', '5c07ab930d6b5', '5c0f0c2a2c71c', 5),
+('616d642a7ccd2', '5c07ab930d6b5', '616d53d7563cc', 5),
+('616d642a7ccd8', '5c07ab930d6b5', '5c0f0c4f5b6af', 5),
+('616d642a7ccdc', '5c07ab930d6b5', '5c0f0c7ee072a', 5),
+('616d642a7cce1', '5c07abb380826', '5c0f0b4f7d558', 5),
+('616d642a7cce6', '5c07abb380826', '5c0f0b6cb7f6a', 5),
+('616d642a7cceb', '5c07abb380826', '5c0f0bd7f11e5', 5),
+('616d642a7ccf0', '5c07abb380826', '5c0f0bf5353b4', 5),
+('616d642a7ccf5', '5c07abb380826', '616d54c781464', 5),
+('616d642a7ccfc', '5c07abb380826', '616d548e74f98', 5),
+('616d642a7cd01', '5c07abb380826', '5c0f0c1481601', 5),
+('616d642a7cd06', '5c07abb380826', '5c0f0c2a2c71c', 5),
+('616d642a7cd0b', '5c07abb380826', '616d53d7563cc', 5),
+('616d642a7cd10', '5c07abb380826', '5c0f0c4f5b6af', 5),
+('616d642a7cd14', '5c07abb380826', '5c0f0c7ee072a', 5),
+('616d642a7cd19', '5c07ac27a9f7f', '5c0f0b4f7d558', 5),
+('616d642a7cd1e', '5c07ac27a9f7f', '5c0f0b6cb7f6a', 5),
+('616d642a7cd22', '5c07ac27a9f7f', '5c0f0bd7f11e5', 5),
+('616d642a7cd27', '5c07ac27a9f7f', '5c0f0bf5353b4', 5),
+('616d642a7cd2c', '5c07ac27a9f7f', '616d54c781464', 5),
+('616d642a7cd31', '5c07ac27a9f7f', '616d548e74f98', 5),
+('616d642a7cd35', '5c07ac27a9f7f', '5c0f0c1481601', 5),
+('616d642a7cd3a', '5c07ac27a9f7f', '5c0f0c2a2c71c', 5),
+('616d642a7cd3f', '5c07ac27a9f7f', '616d53d7563cc', 5),
+('616d642a7cd44', '5c07ac27a9f7f', '5c0f0c4f5b6af', 5),
+('616d642a7cd49', '5c07ac27a9f7f', '5c0f0c7ee072a', 5),
+('616d642a7cd4d', '5c235a78768d1', '5c0f0b4f7d558', 5),
+('616d642a7cd52', '5c235a78768d1', '5c0f0b6cb7f6a', 5),
+('616d642a7cd57', '5c235a78768d1', '5c0f0bd7f11e5', 5),
+('616d642a7cd5c', '5c235a78768d1', '5c0f0bf5353b4', 5),
+('616d642a7cd61', '5c235a78768d1', '616d54c781464', 5),
+('616d642a7cd65', '5c235a78768d1', '616d548e74f98', 5),
+('616d642a7cd6a', '5c235a78768d1', '5c0f0c1481601', 5),
+('616d642a7cd6f', '5c235a78768d1', '5c0f0c2a2c71c', 5),
+('616d642a7cd73', '5c235a78768d1', '616d53d7563cc', 5),
+('616d642a7cd78', '5c235a78768d1', '5c0f0c4f5b6af', 5),
+('616d642a7cd7c', '5c235a78768d1', '5c0f0c7ee072a', 5),
+('616d642a7cd81', '615c83b45a65f', '5c0f0b4f7d558', 5),
+('616d642a7cd86', '615c83b45a65f', '5c0f0b6cb7f6a', 5),
+('616d642a7cd8a', '615c83b45a65f', '5c0f0bd7f11e5', 5),
+('616d642a7cd8f', '615c83b45a65f', '5c0f0bf5353b4', 5),
+('616d642a7cd94', '615c83b45a65f', '616d54c781464', 5),
+('616d642a7cd99', '615c83b45a65f', '616d548e74f98', 5),
+('616d642a7cd9e', '615c83b45a65f', '5c0f0c1481601', 5),
+('616d642a7cda2', '615c83b45a65f', '5c0f0c2a2c71c', 5),
+('616d642a7cda9', '615c83b45a65f', '616d53d7563cc', 5),
+('616d642a7cdaf', '615c83b45a65f', '5c0f0c4f5b6af', 5),
+('616d642a7cdb3', '615c83b45a65f', '5c0f0c7ee072a', 5);
 
 -- --------------------------------------------------------
 
@@ -148,14 +181,17 @@ INSERT INTO `tb_penilaian` (`id_penilaian`, `id_alternatif`, `id_sub_kriteria`, 
 -- Table structure for table `tb_sub_kriteria`
 --
 
-CREATE TABLE `tb_sub_kriteria` (
+DROP TABLE IF EXISTS `tb_sub_kriteria`;
+CREATE TABLE IF NOT EXISTS `tb_sub_kriteria` (
   `id_sub_kriteria` varchar(70) NOT NULL,
   `id_aspek` varchar(70) NOT NULL,
   `kode_sub_kriteria` varchar(50) NOT NULL,
   `nama_sub_kriteria` varchar(150) NOT NULL,
   `nilai_sub_kriteria` int(3) NOT NULL,
   `bobot` int(3) NOT NULL,
-  `keterangan` varchar(3) NOT NULL
+  `keterangan` varchar(3) NOT NULL,
+  PRIMARY KEY (`id_sub_kriteria`),
+  KEY `id_aspek` (`id_aspek`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -163,14 +199,17 @@ CREATE TABLE `tb_sub_kriteria` (
 --
 
 INSERT INTO `tb_sub_kriteria` (`id_sub_kriteria`, `id_aspek`, `kode_sub_kriteria`, `nama_sub_kriteria`, `nilai_sub_kriteria`, `bobot`, `keterangan`) VALUES
-('5c0f0b4f7d558', '5c0a2a2167452', 'JT', 'Jarak Lokasi tempuh dosen ke lokasi penelitian', 5, 60, 'CF'),
-('5c0f0b6cb7f6a', '5c0a2a2167452', 'JL', 'Jarak Lokasi Penelitian dengan Kecamatan atau Kabupaten ', 6, 40, 'SF'),
-('5c0f0bd7f11e5', '5c0a2a3b87d03', 'AK', 'Akses Kendaraan', 6, 70, 'CF'),
-('5c0f0bf5353b4', '5c0a2a3b87d03', 'AJ', 'Akses Jaringan', 5, 30, 'SF'),
-('5c0f0c1481601', '5c0a2a5902c8d', 'BT', 'Biaya Transportasi', 7, 70, 'CF'),
-('5c0f0c2a2c71c', '5c0a2a5902c8d', 'BMM', 'Biaya Makan Minum ', 5, 30, 'SF'),
-('5c0f0c4f5b6af', '5c0a2a7074dc9', 'TP', 'Tempat Penelitian', 6, 50, 'CF'),
-('5c0f0c7ee072a', '5c0a2a7074dc9', 'KDJP', 'Ketersediaan Data yang dibutuhkan dengan Judul Penelitian ', 4, 50, 'SF');
+('5c0f0b4f7d558', '5c0a2a2167452', 'JT', 'Pengangguran', 5, 60, 'CF'),
+('5c0f0b6cb7f6a', '5c0a2a2167452', 'JL', 'Petani', 5, 40, 'SF'),
+('5c0f0bd7f11e5', '5c0a2a3b87d03', 'AK', 'Warga desa', 5, 70, 'CF'),
+('5c0f0bf5353b4', '5c0a2a3b87d03', 'AJ', 'Orang asli Lembor', 4, 30, 'SF'),
+('5c0f0c1481601', '5c0a2a5902c8d', 'PK', 'Penghasilan keluarga', 4, 25, 'CF'),
+('5c0f0c2a2c71c', '5c0a2a5902c8d', 'JAK', 'Jumlah anggota keluarga', 5, 25, 'SF'),
+('5c0f0c4f5b6af', '5c0a2a7074dc9', 'S01', 'Bukan termasuk penerima BNPT', 5, 50, 'CF'),
+('5c0f0c7ee072a', '5c0a2a7074dc9', 'S02', 'Tidak menerima PKH', 5, 50, 'SF'),
+('616d53d7563cc', '5c0a2a7074dc9', 'S03', 'Tidak memiliki kartu prakerja', 4, 20, 'CF'),
+('616d548e74f98', '5c0a2a5902c8d', 'PMK', 'Memiliki anggota keluarga yang rentan sakit menahun/kronis', 5, 25, 'CF'),
+('616d54c781464', '5c0a2a5902c8d', 'DTKS', 'Keluarga miskin yang sudah terdata/belum dalam DTKS)', 4, 25, 'CF');
 
 -- --------------------------------------------------------
 
@@ -178,8 +217,9 @@ INSERT INTO `tb_sub_kriteria` (`id_sub_kriteria`, `id_aspek`, `kode_sub_kriteria
 -- Table structure for table `tb_user`
 --
 
-CREATE TABLE `tb_user` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `tb_user`;
+CREATE TABLE IF NOT EXISTS `tb_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nama` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
@@ -187,8 +227,9 @@ CREATE TABLE `tb_user` (
   `telepon` varchar(15) NOT NULL,
   `tgl_buat` datetime NOT NULL,
   `tgl_ubah` datetime NOT NULL,
-  `status` enum('1','0') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `status` enum('1','0') NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tb_user`
@@ -196,59 +237,6 @@ CREATE TABLE `tb_user` (
 
 INSERT INTO `tb_user` (`id`, `nama`, `email`, `password`, `jenis_kelamin`, `telepon`, `tgl_buat`, `tgl_ubah`, `status`) VALUES
 (1, 'superadmin', 'superadmin@admin.com', '21232f297a57a5a743894a0e4a801fc3', 'L', '085383345262', '2018-12-01 03:10:19', '2018-12-05 02:06:07', '1');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `tb_alternatif`
---
-ALTER TABLE `tb_alternatif`
-  ADD PRIMARY KEY (`id_alternatif`);
-
---
--- Indexes for table `tb_aspek`
---
-ALTER TABLE `tb_aspek`
-  ADD PRIMARY KEY (`id_aspek`);
-
---
--- Indexes for table `tb_hasil`
---
-ALTER TABLE `tb_hasil`
-  ADD PRIMARY KEY (`id_hasil`),
-  ADD KEY `fk_hasil_alternatif` (`id_alternatif`);
-
---
--- Indexes for table `tb_penilaian`
---
-ALTER TABLE `tb_penilaian`
-  ADD KEY `fk_sub_kriteria` (`id_sub_kriteria`),
-  ADD KEY `fk_alternatif` (`id_alternatif`);
-
---
--- Indexes for table `tb_sub_kriteria`
---
-ALTER TABLE `tb_sub_kriteria`
-  ADD PRIMARY KEY (`id_sub_kriteria`),
-  ADD KEY `id_aspek` (`id_aspek`);
-
---
--- Indexes for table `tb_user`
---
-ALTER TABLE `tb_user`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `tb_user`
---
-ALTER TABLE `tb_user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
